4eb3c738c63b07d1d54f95f0db6d41d79dbda6ae
Hunter <hmvitous@yahoo.com>
2020-05-22 17:35:05 +0200

    Builds a robot friend search app

---
+++ src/robots.js
@@ -0,0 +1,62 @@
+export const robots = [
+  {
+    id: 1,
+    name: 'Grace Davis',
+    username: 'Souper',
+    email: 'peepee@poo.com'
+  },
+  {
+    id: 2,
+    name: 'Hunter Vitous',
+    username: 'Awesome',
+    email: 'thecool420@yes.com'
+  },
+  {
+    id: 3,
+    name: 'Joe Mamma',
+    username: 'Joe',
+    email: 'yomamma@ha.com'
+  },
+  {
+    id: 4,
+    name: 'James Barrett',
+    username: 'Monger',
+    email: 'cheeseboy@ost.com'
+  },
+  {
+    id: 5,
+    name: 'Big Dumb',
+    username: 'Me_Smart',
+    email: 'huh@wwhat.com'
+  },
+  {
+    id: 6,
+    name: 'Mrs. Dennis Schulist',
+    username: 'Leopoldo_Corkery',
+    email: 'Karley_Dach@jasper.info'
+  },
+  {
+    id: 7,
+    name: 'Kurtis Weissnat',
+    username: 'Elwyn.Skiles',
+    email: 'Telly.Hoeger@billy.biz'
+  },
+  {
+    id: 8,
+    name: 'Nicholas Runolfsdottir V',
+    username: 'Maxime_Nienow',
+    email: 'Sherwood@rosamond.me'
+  },
+  {
+    id: 9,
+    name: 'Glenna Reichert',
+    username: 'Delphine',
+    email: 'Chaim_McDermott@dana.io'
+  },
+  {
+    id: 10,
+    name: 'Clementina DuBuque',
+    username: 'Moriah.Stanton',
+    email: 'Rey.Padberg@karina.biz'
+  }
+];
