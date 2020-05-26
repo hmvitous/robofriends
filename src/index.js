36dff81dd830ae75836b0484451a15129679ec10
Hunter <hmvitous@yahoo.com>
2020-05-26 13:19:23 +0200

    Implements thunk and creates constants

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from "react-dom";
 import { Provider } from "react-redux";
 import { createStore, applyMiddleware } from "redux";
 import { createLogger } from 'redux-logger'
+import thunkMiddleware from 'redux-thunk'
 import "./index.css";
 import * as serviceWorker from "./serviceWorker";
 import App from "./containers/App";
@@ -10,7 +11,7 @@ import { searchRobots } from "./reducers";
 import "tachyons";
 
 const logger = createLogger()
-const store = createStore(searchRobots, applyMiddleware(logger));
+const store = createStore(searchRobots, applyMiddleware( thunkMiddleware, logger ));
 
 ReactDOM.render(
   <React.StrictMode>
